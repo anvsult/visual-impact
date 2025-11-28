0e6a959477985ce55ad54316a41d0262c64ab643
Pkdotts <andybaole1234@gmail.com>
2025-11-28 04:46:59 -0500

    imported mantine

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,65 +1,44 @@
 import Image from "next/image";
+import '@mantine/core/styles.css';
+import { AppShell, AppShellHeader, AppShellMain, AppShellNavbar, Burger, createTheme, MantineProvider } from '@mantine/core';
 
-export default function Home() {
+const theme = createTheme({
+  colors: {
+    deepBlue: [
+      '#eef3ff', '#dce4f5', '#b9c7e2', '#94a8d0', '#748dc1',
+      '#5f7cb8', '#5474b4', '#44639f', '#39588f', '#2d4b81',
+    ],
+    blue: [
+      '#eef3ff', '#dee2f2', '#bdc2de', '#98a0ca', '#7a84ba',
+      '#6672b0', '#5c68ac', '#4c5897', '#424e88', '#364379',
+    ],
+  },
+  shadows: {
+    md: '1px 1px 3px rgba(0,0,0,.25)',
+    xl: '5px 5px 3px rgba(0,0,0,.25)',
+  },
+  headings: {
+    fontFamily: 'Roboto, sans-serif',
+    sizes: {
+      h1: { fontSize: '60px' },
+      h2: { fontSize: '40px' },
+    },
+  },
+  defaultRadius: 'lg',
+});
+
+function Demo() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+    <MantineProvider theme={theme}>
+      <AppShell
+      header={{ height: 60 }}
+      navbar={{ width: 250, breakpoint: "sm" }}
+    >
+      <AppShellHeader>Header</AppShellHeader>
+      <AppShellNavbar>Navbar</AppShellNavbar>
+      <AppShellMain>Main content</AppShellMain>
+    </AppShell>
+    </MantineProvider>
   );
 }
+export default Demo;
